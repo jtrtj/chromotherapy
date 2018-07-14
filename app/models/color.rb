c8f7a329845988874ada5acddb71cacfcdda25ef
jtrtj <jt.r@live.com>
2018-07-14 12:54:05 -0600

    add relationships to models and pass tests

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,4 +1,7 @@
 class Color < ApplicationRecord
   validates_presence_of :name, :admin_id
   validates :hex_value, presence: true, uniqueness: { case_sensitive: false }
+
+  belongs_to :admin
+  
 end
