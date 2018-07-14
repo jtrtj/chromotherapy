af4a2025d79406874930fadf7fbcdb26895bd5ac
jtrtj <jt.r@live.com>
2018-07-14 12:22:33 -0600

    add authenticate method to admin class and database cleaner

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   gem 'pry'
   gem 'active_designer'
   gem 'simplecov'
+  gem 'database_cleaner'
 end
 
 group :development do
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -7,6 +7,16 @@ describe Admin do
     it {should validate_presence_of(:email)}
     it {should validate_presence_of(:password)}
 
-    it {should validate_uniqueness_of(:screen_name)}
+    it {should validate_uniqueness_of(:screen_name).case_insensitive}
+    it {should validate_uniqueness_of(:email).case_insensitive}
+  end
+
+  context 'class methods' do
+    it '#authenticate' do
+      admin = Admin.create!(name: 'John', screen_name: 'jtr', email: 'jtr022@gmail.com', password: 'cool')
+
+      expect(Admin.authenticate('jtr', 'cool')).to eq(admin)
+      expect(Admin.authenticate('jtr', 'chg')).to eq(nil)
+    end
   end
 end
