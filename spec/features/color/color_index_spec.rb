4780019bbb58244151b4a34331e2e7ddd1379afb
jtrtj <jt.r@live.com>
2018-07-15 12:20:23 -0600

    add links to colors in index make test for that but not sure exactly how to test it

---
+++ spec/features/color/color_index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'a user' do
+  context 'visiting colors index' do
+    xit "sees all colors and can click on one to see it's show page" do
+      admin = Admin.create!(name: 'John', screen_name: 'jtr', email: 'jtr022@gmail.com', password: 'cool')
+      bmp = admin.colors.create!(name: 'baker-miller-pink', hex_value: '#ff91af')
+      black = admin.colors.create!(name: 'black', hex_value: '#000000')
+
+      visit colors_path
+    
+      expect(page).to have_css("#{bmp.hex_value}")
+      expect(page).to have_css("#{black.hex_value}")
+    
+     within "#color-#{bmp.id}" do
+      click_on 
+     end
+     
+     expect(current_path).to be(color_path(bmp))
+    end
+  end
+end
