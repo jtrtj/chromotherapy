4dab16b78b8f5079091ae9ca9b3711d053acb1d0
jtrtj <jt.r@live.com>
2018-07-16 14:38:05 -0600

    pass user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :name, require: true
+  validates_presence_of :password, require: true
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe User do
   describe 'validations' do
     it {should validate_presence_of(:email)}
-    it {should validate_uniqueness_of(:emial)}
+    it {should validate_uniqueness_of(:email)}
 
     it {should validate_presence_of(:name)}
     
