d276e3de74992519e453cc05fef597268f91c545
jtrtj <jt.r@live.com>
2018-07-17 11:11:47 -0600

    moving on to adding roles to users

--- spec/features/color/color_index_spec.rb
+++ spec/features/color/color_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'a user' do
   context 'visiting colors index' do
-    xit "sees all colors and can click on one to see it's show page" do
+    it "sees all colors and can click on one to see it's show page" do
       admin = Admin.create!(name: 'John', screen_name: 'jtr', email: 'jtr022@gmail.com', password: 'cool')
       bmp = admin.colors.create!(name: 'baker-miller-pink', hex_value: '#ff91af')
       black = admin.colors.create!(name: 'black', hex_value: '#000000')
@@ -12,7 +12,7 @@ describe 'a user' do
       # expect(page).to have_css("#{bmp.hex_value}")
       # expect(page).to have_css("#{black.hex_value}")
     
-      find("#color-#{bmp.id}").click
+    click_on "#color-#{bmp.id}"
     
      
      expect(current_path).to be(color_path(bmp))
