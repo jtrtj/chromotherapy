3e893d13372582b5634f6c49ecc801ec8b215375
jtrtj <jt.r@live.com>
2018-07-18 11:38:49 -0600

    add two colors to set up for many tests so that rspec can generate gradients

--- spec/features/color/color_index_spec.rb
+++ spec/features/color/color_index_spec.rb
@@ -2,20 +2,17 @@ require 'rails_helper'
 
 describe 'a user' do
   context 'visiting colors index' do
-    xit "sees all colors and can click on one to see it's show page" do
-      admin = Admin.create!(name: 'John', screen_name: 'jtr', email: 'jtr022@gmail.com', password: 'cool')
+    it "sees all colors and can click on one to see it's show page" do
+      admin = User.create!(name: 'John', email: 'jtr022@gmail.com', password: 'cool')
       bmp = admin.colors.create!(name: 'baker-miller-pink', hex_value: '#ff91af')
       black = admin.colors.create!(name: 'black', hex_value: '#000000')
 
       visit colors_path
     
-      # expect(page).to have_css("#{bmp.hex_value}")
-      # expect(page).to have_css("#{black.hex_value}")
-    
-    click_on "#color-#{bmp.id}"
+      click_on "#{bmp.name}"
     
      
-     expect(current_path).to be(color_path(bmp))
+      expect(current_path).to eq(color_path(bmp))
     end
   end
 
